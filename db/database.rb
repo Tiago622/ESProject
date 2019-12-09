c974acf2489e28d4405926877efe386e61cb97fb
Fmpalves <58446653+Fmpalves@users.noreply.github.com>
2019-12-09 19:05:56 +0000

    Add files via upload

---
+++ db/database.rb
@@ -0,0 +1,21 @@
+class Createlesson < ActiveRecord::Migration
+	def change
+		create_table :lesson do |t|
+			t.datetime :data_inicio
+			t.datetime :data_fim
+			t.string :tipo
+			t.int :Ano
+			t.int :Sementre
+			t.int :turma
+			t.string :dia_semana
+			t.int :estado
+			t.string :ambito 
+			t.string :curso
+			t.string :lesson
+			t.sting :responsavel 
+			t.datetime :data
+			t.text :motivo_rejeicao
+         end
+    end
+
+end
