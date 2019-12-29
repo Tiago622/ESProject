94712f4e53a6c302dd670fdd672615ca945f88c9
DarVoid <jorgemduque@hotmail.com>
2019-12-29 21:40:47 +0000

    robots.txt
    only our lord and Savior Google can crawl our website now

--- pdfreader/getSalas.rb
+++ pdfreader/getSalas.rb
@@ -1,55 +1,60 @@
-require 'open-uri'
-require 'nokogiri'
-require 'json'
 
-def fromStringArrayOfObjectsToArrayOfObjects(colecao)
-    colec=[]
-    n=0
-    begin
-        colecao.split("},{").each do |entrada|
-            
-            if n == 0
-                entrada = entrada[2..]
+#module Blog
+    #class PostsController < Blog::ApplicationController
+        require 'open-uri'
+        require 'nokogiri'
+        require 'json'
+        def fromStringArrayOfObjectsToArrayOfObjects(colecao)
+            colec=[]
+            n=0
+            begin
+                colecao.split("},{").each do |entrada|
+                    
+                    if n == 0
+                    entrada = entrada[2..-1]
+                    end
+                    entrada = entrada.split(",\"FuncaoFR\":")[0]
+                    nome_sala = entrada.split(",\"Nome\":\"")[1].split("\",\"FuncaoPT\":\"")[0]
+                    description = entrada.split(",\"Nome\":\"")[1].split("\",\"FuncaoPT\":\"")[1]
+                    #puts nome_sala 
+                    #puts description
+                    cena= {:nome=>nome_sala,:description=>description.split("\"")[0]}
+                    colec.push(cena)
+                    puts cena
+                    
+                    #puts entrada
+                    #puts type(entrada)
             end
-            entrada = entrada.split(",\"FuncaoFR\":")[0]
-            nome_sala = entrada.split(",\"Nome\":\"")[1].split("\",\"FuncaoPT\":\"")[0]
-            description = entrada.split(",\"Nome\":\"")[1].split("\",\"FuncaoPT\":\"")[1]
-            #puts nome_sala 
-            #puts description
-            cena= {:nome=>nome_sala,:description=>description.split("\"")[0]}
-            colec.push(cena)
-            puts cena
-            
-            #puts entrada
-            #puts type(entrada)
-    end
-    rescue NoMethodError => e
-       return
-    end    
-    return colec
-
-end
-
-
-lista_de_salas=[]
-nome_ficheiro="salas.txt"
-base_url = "http://appmobile.ipt.pt/api/salasapi"
-letras=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-
-filename_base="salas"
-letras.each do |letra|
-    4.times do |andar|
-        url = base_url + "\/#{letra}\/#{andar}"
-        puts url
-        doc = Nokogiri::HTML(open(url))
-        doc = doc.xpath('//p').to_s[3..-5]
-        puts doc
-        fromStringArrayOfObjectsToArrayOfObjects(doc)
-        #Space.create(^)
+            rescue NoMethodError => e
+               return
+            end    
+            return colec
         
-    end
-end
-
-lista_de_salas.each do |sala|
-    Space.create({:name=> sala.split(",")[0] , :description=>sala.split(",")[1]})
-end
+        end
+        
+        
+        lista_de_salas=[]
+        nome_ficheiro="salas.txt"
+        base_url = "http://appmobile.ipt.pt/api/salasapi"
+        letras=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+        
+        filename_base="salas"
+        letras.each do |letra|
+            4.times do |andar|
+                url = base_url + "\/#{letra}\/#{andar}"
+                puts url
+                doc = Nokogiri::HTML(open(url))
+                doc = doc.xpath('//p').to_s[3..-5]
+                puts doc
+                a = fromStringArrayOfObjectsToArrayOfObjects(doc)
+                #Space.create(a)
+                
+            end
+        end
+        
+        lista_de_salas.each do |sala|
+           # Space.create({:name=> sala.split(",")[0] , :description=>sala.split(",")[1]})
+        end
+        
+    #end
+ # end
