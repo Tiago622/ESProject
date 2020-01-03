5daba380ff0f2b0ef8db9dae1f2e106cd06d1da3
DarVoid <jorgemduque@hotmail.com>
2020-01-03 01:18:15 +0000

    Revert "getSalas melhor e getPeople"
    This reverts commit dddf6fab1dfe007d40cafe073f32010f8cee23ef.

--- pdfreader/insertPeople.rb
+++
@@ -1,9 +0,0 @@
-require 'csv'
-ficheiro = File.read("pessoas.csv")
-
-table = CSV.parse(ficheiro, headers: true)
-
-table.each do |linha|
-    Person.create({:name=>linha['nome']})
-    puts "#{linha['nome']} : #{linha['email']}"
-end
