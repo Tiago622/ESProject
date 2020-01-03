9c3fdd9dde9204fadeddf1c3068fe961668030b7
João Ferreira <aluno19164@ipt.pt>
2020-01-03 15:19:18 +0000

    Seed
    Seed
    
    Co-Authored-By: tiago622 <tiago622@users.noreply.github.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+OrganicUnit.create([
+    {name: 'Instituto Politécnico de Tomar', acronym: 'IPT'}
+])
+
+School.create([
+    {name:'Escola Superior de Gestão de Tomar', acronym:'ESGT', address:"Quinta do Contador, Estrada da Serra, 2300 Tomar, Portugal", organic_unit_id:'1'},
+    {name:'Escola Superior de Tecnologia de Abrantes', acronym:'ESTA', address:"Rua 17 de Agosto de 1808 s/n
+        2200-370 Abrantes", organic_unit_id:'1'},
+    {name:'Escola Superior de Tecnologia de Tomar', acronym:'ESTT', address:"Quinta do Contador, Estrada da Serra, 2300 Tomar, Portugal", organic_unit_id:'1'}
+])
