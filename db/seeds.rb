f51c48d8eeb638d9614caaeba0c65a75c69d6764
João Ferreira <aluno19164@ipt.pt>
2020-01-04 13:36:25 +0000

    Update seed
    Adição de seeds de Course

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,13 @@ OrganicUnit.create([
 
 School.create([
     {name:'Escola Superior de Gestão de Tomar', acronym:'ESGT', address:"Quinta do Contador, Estrada da Serra, 2300 Tomar, Portugal", organic_unit_id:'1'},
-    {name:'Escola Superior de Tecnologia de Abrantes', acronym:'ESTA', address:"Rua 17 de Agosto de 1808 s/n
-        2200-370 Abrantes", organic_unit_id:'1'},
+    {name:'Escola Superior de Tecnologia de Abrantes', acronym:'ESTA', address:"Rua 17 de Agosto de 1808 s/n 2200-370 Abrantes", organic_unit_id:'1'},
     {name:'Escola Superior de Tecnologia de Tomar', acronym:'ESTT', address:"Quinta do Contador, Estrada da Serra, 2300 Tomar, Portugal", organic_unit_id:'1'}
+])
+
+Course.create([
+    {name:'Gestão de Recursos Humanos e Comportamento Organizacional', school_id:'1'},
+    {name:'Contabilidade e Gestão', school_id:'1'},
+    {name:'Licenciatura em Engenharia Informática', school_id:'3'},
+    {name:'Licenciatura em Engenharia Eletrotécnica e de Computadores', school_id:'3'}
 ])
