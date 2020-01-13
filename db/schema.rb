a75a442a1bc23a4ab54b58a75a27675f0702bf89
João Ferreira <aluno19164@ipt.pt>
2020-01-13 21:38:38 +0000

    Finished connecting N-N
    Co-Authored-By: tiago622 <tiago622@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_223145) do
+ActiveRecord::Schema.define(version: 2020_01_10_152143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2020_01_06_223145) do
     t.index ["discipline_id"], name: "index_lessons_on_discipline_id"
     t.index ["person_id"], name: "index_lessons_on_person_id"
     t.index ["space_id"], name: "index_lessons_on_space_id"
+  end
+
+  create_table "lessons_schedules", id: false, force: :cascade do |t|
+    t.bigint "lesson_id"
+    t.bigint "schedule_id"
+    t.index ["lesson_id"], name: "index_lessons_schedules_on_lesson_id"
+    t.index ["schedule_id"], name: "index_lessons_schedules_on_schedule_id"
   end
 
   create_table "organic_units", force: :cascade do |t|
@@ -130,6 +137,19 @@ ActiveRecord::Schema.define(version: 2020_01_06_223145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_urls_on_course_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "courses", "schools"
