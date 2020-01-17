b73654e0d2c6375d2bae0cdd8eb92ac9ce43e4b3
João Ferreira <aluno19164@ipt.pt>
2020-01-17 13:45:28 +0000

    Scaffold roles; Added FK role to Users; Automatically give the role to a new registered user; Re-generate erd model; Seed roles; Changes to layout header

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+  belongs_to :role
+  before_validation :set_default_role 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+  def set_default_role
+    self.role ||= Role.find_by_role('Registered')
+  end
+
 end
