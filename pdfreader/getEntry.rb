82a1e50984d4f67aac61c493c3e2fbdefbc48a8b
DarVoid <jorgemduque@hotmail.com>
2020-01-17 14:31:51 +0000

    schedules
    schedules

--- pdfreader/getEntry.rb
+++ pdfreader/getEntry.rb
@@ -1,12 +1,6 @@
 require 'byebug'
 queue = ["SEGUNDA", "TERCA", "QUARTA", "QUINTA", "SEXTA"] #, "SABADO" #meter quando houve horarios com sabado
-
-segunda_array= []
-terca_array = []
-quarta_array = []
-quinta_array = []
-sexta_array = []
-sabado_array = []
+arrays=[]
 
 
 unidade= {:name=>"IPT", :acronym=> "IPT"}
@@ -32,15 +26,21 @@ queue.each do |ficheiro_dia_da_semana|
       linha = line.split('/')
       semestre = "#{linha[1].split(": ")[1]}"
       ano_lec = "#{linha[2].split(": ")[1]}"
+      puts "ano_lec:"
+      puts ano_lec
+      puts "######"
       turma = "#{linha[3].split(": ")[1]}" #turma
+      
       ano_curso = "#{linha[4].split(": ")[1]}" #ano
+      puts "ano_curso:"
+      puts ano_curso
+      puts "######"
       dia_s = "#{linha[6].split(": ")[1]}" #dia da semana
       curso= "#{linha[5].split(": ")[1]}" #curso
       @cursoe = Course.find_by(:name=>curso) 
       if @cursoe.nil?
         @cursoe = Course.create(:name=>curso, :school=>@escolae)
       end
-      
       mensagem = "#{linha[8].split(": ")[1]}" #texto
       
       if line_num %4 == 1
@@ -51,40 +51,17 @@ queue.each do |ficheiro_dia_da_semana|
         sala = mensagem.split("\r\n")[0]
         @salae = Space.find_by(:name=>sala)
         if @salae.nil?
-          @salae =Space.create({:name=>sala,:description=>sala}) 
+          #@salae =Space.create({:name=>sala,:description=>sala}) 
           puts sala
         end
-
-        case (dia_s)
-          when "SEGUNDA" 
-            segunda_array.push({:lesson_type => tipo, :week_day=>dia_s, :end_time => hora_fim, :space=> @salae, :person=> @responsavele, :start_time => hora_inicio, :name => cadeira, :discipline => @disc}) #:semestre => semestre, :course => @curso, :ano_curso => ano_curso, :turma => turma, :ano_lec => ano_lec
-            puts "#{segunda_array}"
-          when "TERCA"
-            terca_array.push({:lesson_type => tipo, :week_day=>dia_s, :end_time => hora_fim, :space=> @salae, :person=> @responsavele,:start_time => hora_inicio  , :name => cadeira, :discipline => @disc})
-            puts "#{terca_array}"
-          when "QUARTA"
-            quarta_array.push({:lesson_type => tipo, :week_day=>dia_s, :end_time => hora_fim, :space=> @salae, :person=> @responsavele,:start_time => hora_inicio  , :name => cadeira, :discipline => @disc})
-            puts "#{quarta_array}"
-          when "QUINTA"
-            quinta_array.push({:lesson_type => tipo, :week_day=>dia_s, :end_time => hora_fim, :space=> @salae, :person=> @responsavele,:start_time => hora_inicio  , :name => cadeira, :discipline => @disc})
-            puts "#{quinta_array}"
-          when "SEXTA"
-            sexta_array.push({:lesson_type => tipo, :week_day=>dia_s, :end_time => hora_fim, :space=> @salae, :person=> @responsavele,:start_time => hora_inicio  , :name => cadeira, :discipline => @disc})
-            puts "#{sexta_array}"
-          when "SABADO"
-            sabado_array.push({:lesson_type => tipo, :week_day=>dia_s, :end_time => hora_fim, :space=> @salae, :person=> @responsavele,:start_time => hora_inicio  , :name => cadeira, :discipline => @disc})
-            puts "#{sabado_array}"
-        end
-        
+        @less = Lesson.create({:lesson_type => tipo, :week_day=>dia_s, :end_time => hora_fim, :space=> @salae, :person=> @responsavele, :start_time => hora_inicio, :name => cadeira, :discipline => @disc}) #:semestre => semestre, :course => @curso, :ano_curso => ano_curso, :turma => turma, :ano_lec => ano_lec
       elsif line_num %4 == 2
         responsavel = mensagem.split("\r\n")[0]
         @responsavele = Person.find_by(:name=>responsavel) 
         if @responsavele.nil?
           puts responsavel
           @responsavele = Person.create(:name=>responsavel)
-
         end
-        
       else
         hora_inicio = "#{linha[7].split(": ")[1].split(" - ")[0]}" #horas inicio
         hora_inicio = hora_inicio.split(".").join(":")+":00"
@@ -94,39 +71,27 @@ queue.each do |ficheiro_dia_da_semana|
           @disc=Discipline.create(:name=>cadeira, :course=>@cursoe)
         end
       end
-    
-    
-   
       line_num += 1
-     
+      
+      @horario = Schedule.find_by({:school_year=>ano_curso, :year=>ano_lec, :schedule_class=> turma, :course=> @cursoe})
+      if @horario.nil?
+        @horario = Schedule.create({:school_year=>ano_curso, :year=>ano_lec, :schedule_class=> turma, :version=>"1", :course=> @cursoe})
+      else
+        pp @horario
+      end
+      LessonSchedule.create(:lesson=>@less, :schedule=>@horario)
+
   end
 end
 
-arrays= [segunda_array,terca_array,quarta_array,quinta_array,sexta_array,sabado_array]
+#arrays= [segunda_array,terca_array,quarta_array,quinta_array,sexta_array,sabado_array]
+#puts arrays.size
+#puts arrays.uniq.size
+arrays.each do |entrada|
+  
+ # pp entrada
 
-#disciplinas = disciplinas.uniq
-#cursos= cursos.uniq
-#people= people.uniq
-
-
-#cursos.each do |curso|
-  #puts curso
-  #Course.create({:name=> curso, :school=>@escolae})
-#end
-
-#disciplinas.each do |disciplina|
-  #puts disciplina
-  #Discipline.create(disciplina)
-
-#end
-
-#people.each do |person|
-  #Person.create(person)
-#end
-arrays.each do |dia|
-  dia.each do |entrada|
-    puts entrada
-    @less = Lesson.create(entrada)
-    # byebug
-  end
+#  
+  # byebug
+  
 end
